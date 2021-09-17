1b09de8dacc842e262a9752924c1d611e0abd519
Clint Ngo <clintngo@gmail.com>
2021-09-17 16:38:13 -0500

    Commit

---
+++ JS Button Clicker/script.js
@@ -0,0 +1,9 @@
+function login(element) {
+    element.innerText = "Logout";
+}
+function remove(element) {
+    element.remove();
+}
+function like(element) {
+    alert("Ninja was Liked", element);
+}

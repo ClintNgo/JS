eb84435e58db657c7582a79f1b7d822754d184d5
Clint Ngo <clintngo@gmail.com>
2021-09-17 13:14:52 -0500

    commit

---
+++ JS Loop Challenges/script1.js
@@ -0,0 +1,27 @@
+for(var i = 1; i<20; i++){
+    if(i % 2 == 1)
+    console.log(i)
+}
+
+for(var i = 100; i>0; i--){
+    if (i % 3==0)
+    console.log(i)
+}
+
+var i = 4
+    while (i > -4) {
+        console.log(i);
+        i-=1.5;
+}
+
+var total = 0
+    for (var i = 0; i<101; i++){
+        total+=i;
+}
+    console.log (total);
+
+var total = 1
+    for (var i = 1; i<13; i++){
+    total*=i;
+}
+    console.log (total);
